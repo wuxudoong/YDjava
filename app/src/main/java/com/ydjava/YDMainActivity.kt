1d531ca5f3ad5a589869f76a93e6bd2e389c2c16
wuxudoong <17826875948@163.com>
2018-09-10 00:41:42 +0800

    初始化

---
+++ app/src/main/java/com/ydjava/YDMainActivity.kt
@@ -0,0 +1,15 @@
+package com.ydjava
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.app_ydmain_activity.*
+
+class YDMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.app_ydmain_activity)
+
+    }
+
+}
